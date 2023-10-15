165517ff6626304940eee569654180a62cdb2811
dina <dinahamadi.20@gmail.com>
2023-10-16 00:50:17 +0100

    fixing %s

--- funcp3o5.c
+++ funcp3o5.c
@@ -10,7 +10,7 @@ int str_print(const char *format, va_list args, int *idx)
 {
 	char *arg = (char *)va_arg(args, char*);
 
-	(void)gormat;
+	(void)format;
 	if (arg == NULL || *arg == '\0')
 	{
 		_putchar('(');
