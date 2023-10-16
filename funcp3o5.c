8491fdf84f38e07112ab59d6b9acd51e0fb6ccbe
dina <dinahamadi.20@gmail.com>
2023-10-16 07:05:44 +0100

    fixing 0

--- funcp3o5.c
+++ funcp3o5.c
@@ -11,7 +11,7 @@ int str_print(const char *format, va_list args, int *idx)
 	char *arg = (char *)va_arg(args, char *);
 
 	(void)format;
-	if (arg == NULL)
+	if (arg == NULL || *arg == '\0')
 	{
 		(*idx)++;
 		return (print_null());
