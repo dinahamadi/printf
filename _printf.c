5930257f0cc33c71d66a0193d569e582fbeadac3
dina <dinahamadi.20@gmail.com>
2023-10-16 03:03:49 +0100

    fixing %s

--- _printf.c
+++ _printf.c
@@ -12,7 +12,7 @@ int _printf(const char *format, ...)
 	va_start(args, format);
 	if (format == NULL)
 		return (-1);
-	while (format[idx] != '\0')
+	while (format[idx] && (format[idx] != '\0'))
 	{
 		if (format[idx] != '%')
 		{
