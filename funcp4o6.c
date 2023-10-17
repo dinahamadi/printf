814c721ae8e674e7066884f98110b4e46b0a515f
dina <dinahamadi.20@gmail.com>
2023-10-17 06:56:05 +0100

    fixing %+

--- funcp4o6.c
+++ funcp4o6.c
@@ -65,8 +65,36 @@ int l_align_print(const char *format, va_list args, int *idx)
  */
 int sign_print(const char *format, va_list args, int *idx)
 {
-	(void)idx;
+	int res = 0, i = 0, arg = va_arg(args, int), digit[10];
+
 	(void)format;
-	(void)args;
-	return (1);
+	if (args == NULL)
+		return (-1);
+	if (arg == 0)
+	{
+		(*idx) += 2;
+		return (write(1, "+0", 2));
+	}
+	if (arg < 0)
+	{
+		res += _putchar('-');
+		arg = (-1) * arg;
+	}
+	else
+		res += _putchar('+');
+	if (arg == INT_MIN)
+	{
+		_putchar('2');
+		arg = 147483648;
+		res++;
+	}
+	while (arg > 0)
+	{
+		digit[i++] = arg % 10;
+		arg /= 10;
+	}
+	while (i > 0)
+		res += _putchar('0' + digit[--i]);
+	(*idx) += 2;
+	return (res);
 }
