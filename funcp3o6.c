d925996e2b6d9ecbf2bb3ace4f5e32ecd6468da5
dina <dinahamadi.20@gmail.com>
2023-10-17 07:31:17 +0100

    fixing %s

--- funcp3o6.c
+++ funcp3o6.c
@@ -10,8 +10,7 @@ int str_print(const char *format, va_list args, int *idx)
 {
 	char *arg = (char *)va_arg(args, char *);
 
-	(void)format;
-	if (args == NULL)
+	if (args == NULL || format == NULL)
 		return (-1);
 	if (arg == NULL)
 	{
